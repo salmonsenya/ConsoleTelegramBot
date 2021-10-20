22c36b4e0aaa8bc62f285b67de41248818d3529d
Kseniia Khromova <kseniiakhromova@yandex.ru>
2021-10-20 16:54:15 +0300

    Initial commit.

---
+++ ConsoleTelegramBot/Program.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+
+namespace ConsoleTelegramBot
+{
+    class Program
+    {
+        static IConfigurationRoot Configuration;
+        static ITelegramBotClient botClient;
+
+        static async Task Main(string[] args)
+        {
+            Configuration = new ConfigurationBuilder().SetBasePath(ConfigurationPath.Combine(AppContext.BaseDirectory))
+                .AddJsonFile("appsettings.json", optional: false).Build();
+            string token = Configuration.GetSection("BotConfiguration")?.GetSection("BotToken")?.Value ?? throw new ArgumentNullException("Failed to get a token from a configuration.");
+
+            botClient = new TelegramBotClient(token);
+            var info = await botClient.GetMeAsync();
+            Console.WriteLine($"Telegram bot {info.FirstName} is started.");
+
+            botClient.OnMessage += Bot_OnMessage;
+            botClient.StartReceiving();
+
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+
+            botClient.StopReceiving();
+        }
+
+        static async void Bot_OnMessage(object sender, MessageEventArgs e)
+        {
+            var input = e.Message.Text;
+            if (input != null)
+            {
+                var regexYes = new Regex(@"(^((да)|(Да)|(ДА))(\?*|\.*|!*)$)|(((\s|,)да\?+)$)");
+                if (regexYes.IsMatch(input))
+                {
+                    await botClient.SendTextMessageAsync(
+                        chatId: e.Message.Chat.Id,
+                        replyToMessageId: e.Message.MessageId,
+                        text: "пизда");
+                }
+
+                var regexNo = new Regex(@"(^((нет)|(Нет)|(НЕТ))(\.*|!*)$)");
+                if (regexNo.IsMatch(input))
+                {
+                    await botClient.SendTextMessageAsync(
+                        chatId: e.Message.Chat.Id,
+                        replyToMessageId: e.Message.MessageId,
+                        text: "пидора ответ");
+                }
+            }
+        }
+    }
+}
---
+++ ConsoleTelegramBot/README.md
@@ -0,0 +1,6 @@
+﻿### About
+Прототип Telegram бота на C#. Kонсольное приложение с использованием клиента Telegram.Bot. Принимает обновления через getUpdates. 
+### Setup
+* Добавьте token, полученный от BotFather, в appsettings.json.
+* Хороший [пост](https://habr.com/ru/company/microsoft/blog/435914/) на хабре про то, как упаковать .NET Core с помощью Docker
+* Мой [гайд](https://salmonsenya.github.io/DiscourteousBotWebhook/) о том, как написать Telegram бот на ASP.Net Core и раскатить его на Linux сервере
